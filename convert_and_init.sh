bab14c25bb7d7db413946cf8bb0cc8771bb2a437
GU Junhua <jhgu@nao.cas.cn>
2022-05-06 20:51:58 +0800

    modified:   convert_and_init.sh 	modified:   selfcal.sh

--- convert_and_init.sh
+++ convert_and_init.sh
@@ -10,17 +10,21 @@
 
 source ~/.bashrc
 
-PREFIX=./ms_
-DATE=20130717
-CH='6144:6400'
+OUTPUT_PATH="./"
+DATA_PATH="./"
+CONTAINER_PATH="./"
 INPUT_PATH='/data/21cma/disk1/'
+
+PREFIX="ms_"
+DATE="20130717"
+CH='6144:6400'
 INIT_MODEL="components.cl"
 SOL_INT="5min"
 REF_ANT="E01"
 
 
-singularity exec -B ${INPUT_PATH} ../casa.simg /usr/local/bin/raw2ms_splited ./ANTENNA/ ${PREFIX} ${DATE} ${INPUT_PATH} ${CH}
+singularity exec -B ${INPUT_PATH} ${CONTAINER_PATH}/casa.simg /usr/local/bin/raw2ms_splited ${DATA_PATH}/ANTENNA/ ${OUTPUT_PATH}/${PREFIX} ${DATE} ${INPUT_PATH} ${CH}
 
-singularity exec ../casa.simg casa --nogui --logfile ./a.log -c ./init_process_cmd.py ${PREFIX}${CH} ${INIT_MODEL} ${SOL_INT} ${REF_ANT}
+singularity exec ${CONTAINER_PATH}/casa.simg casa --nogui --logfile ${OUTPUT_PATH}/a.log -c ./init_process_cmd.py ${OUTPUT_PATH}/${PREFIX}${CH} ${DATA_PATH}/${INIT_MODEL} ${SOL_INT} ${REF_ANT}
 
 #singularity exec ../casa.simg casa --nogui --logfile ./a.log -c ./selfcal.py ${PREFIX}${CH} ${SOL_INT} ${REF_ANT}
--- selfcal.sh
+++ selfcal.sh
@@ -10,17 +10,13 @@
 
 source ~/.bashrc
 
-PREFIX=./ms_
-DATE=20130717
+OUTPUT_PATH="./"
+CONTAINER_PATH="./"
+PREFIX="ms_"
+
 CH='6144:6400'
-INPUT_PATH='/data/21cma/disk1/'
-INIT_MODEL="components.cl"
 SOL_INT="5min"
 REF_ANT="E01"
 
 
-#singularity exec ../casa.simg /usr/local/bin/raw2ms_splited ./ANTENNA/ ${PREFIX} ${DATE} ${INPUT_PATH} ${CH}
-
-#singularity exec ../casa.simg casa --nogui --logfile ./a.log -c ./init_process_cmd.py ${PREFIX}${CH} ${INIT_MODEL} ${SOL_INT} ${REF_ANT}
-
-singularity exec ../casa.simg casa --nogui --logfile ./a.log -c ./selfcal.py ${PREFIX}${CH} ${SOL_INT} ${REF_ANT}
+singularity exec ${CONTAINER_PATH}/casa.simg casa --nogui --logfile ${OUTPUT_PATH}/a.log -c ./selfcal.py ${OUTPUT_PATH}/${PREFIX}${CH} ${SOL_INT} ${REF_ANT}
